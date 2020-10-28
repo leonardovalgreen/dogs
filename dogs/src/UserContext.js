5a603fe08cdc1382a9a28a2f04fd4d95658c3dd2
leonardovalverderadarfit <70704988+leonardovalverderadarfit@users.noreply.github.com>
2020-10-28 14:26:31 -0300

    !
    !

--- dogs/src/UserContext.js
+++ dogs/src/UserContext.js
@@ -37,7 +37,8 @@ export const UserStorage = ({ children }) => {
       setLoading(true);
       const { url, options } = TOKEN_POST({ username, password });
       const tokenRes = await fetch(url, options);
-      if (!tokenRes) throw new Error(`Error: ${tokenRes.statusText}`);
+      console.log(tokenRes);
+      if (!tokenRes.ok) throw new Error(`Error: ${tokenRes.statusText}`);
       const { token } = await tokenRes.json();
       window.localStorage.setItem("token", token);
       await getUser(token);
