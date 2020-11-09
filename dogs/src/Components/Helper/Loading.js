1ea5863c569649de2d2c7e99ae9bd3d687fcabb3
leonardovalverderadarfit <70704988+leonardovalverderadarfit@users.noreply.github.com>
2020-11-09 14:31:14 -0300

    feat: feed modal

--- dogs/src/Components/Helper/Loading.js
+++ dogs/src/Components/Helper/Loading.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
 const Loading = () => {
-  return <div>carregando...</div>;
+  return <div>Carregando...</div>;
 };
 
 export default Loading;
